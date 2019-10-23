09130d273297a0c9024c6fd0ca3dc91d22eeafdd
lookwendyican <wendyalers@gmail.com>
2019-10-22 21:52:20 -0400

    initial files

---
+++ wonders/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+const Header = props => (
+  <div className="Header">
+    <ul>
+      <li>
+        <h2 className="title animated swing">Clicky Game</h2>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Header;
